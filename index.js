41e6853db8a3b96263f84a89ab88e8dbc389cdf9
lenovo <test@qq.com>
2017-03-06 21:42:01 +0800

    init project

---
+++ index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { DatePicker } from 'antd';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
+
+const RangePicker = DatePicker.RangePicker;
+
+
+function disabledDate(current) {
+    const dateFormat = 'YYYY/MM/DD';
+    return current && current.valueOf() < Date.now() || current && current.valueOf() > moment('2017/09/01', dateFormat);
+}
+
+function App() {
+  return (
+    <div style={{ margin: 100 }}>
+        <DatePicker
+            format="YYYY-MM-DD"
+            disabledDate={disabledDate}
+            showTime
+            />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
